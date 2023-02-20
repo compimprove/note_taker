3393eea47b262995327566ac3227805ddd0d82e8
dinhnt <compimprove@gmail.com>
2023-02-20 08:22:03 +0700

    Add logging

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ require('dotenv').config({ path: './.env' });
 const express = require('express');
 const noteRouter = require('./src/router/noteRouter');
 const mongoose = require('mongoose');
+const morgan = require('morgan')
 const { errorResponder, errorLogger } = require('./src/middleware/errorHandler');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(morgan(':date[clf] :method :url :status - :response-time ms'))
 app.use('/api/notes', noteRouter)
 app.use(errorLogger);
 app.use(errorResponder);
